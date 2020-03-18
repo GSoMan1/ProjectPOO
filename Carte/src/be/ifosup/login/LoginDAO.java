23b31cd629b49a95284a0b41b060ce36c3939bc8
Kinoxbe <56653418+Kinoxbe@users.noreply.github.com>
2020-03-18 21:00:52 +0100

    Update LoginDAO.java

--- Carte/src/be/ifosup/login/LoginDAO.java
+++ Carte/src/be/ifosup/login/LoginDAO.java
@@ -3,56 +3,53 @@ package be.ifosup.login;
 import java.sql.*;
 
 public class LoginDAO {
-    public static boolean valid(String nom,String pass) throws SQLException {
-        boolean action = false;
+    protected static void initializeDatabase()
+            throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver Ok");
-        }
-        catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        String url="jdbc:mysql://localhost:3306/5IPO1?serverTimezone=UTC";
-        String user="root";
-        String password="";
-        Connection connection=null;
-        Statement statement=null;
-        ResultSet resultat=null;
+        String url = "jdbc:mysql://localhost:3306/5IPO1?serverTimezone=UTC";
+        String user = "root";
+        String password = "";
+        Connection connection = null;
+//        Statement statement=null;
+//        ResultSet resultat=null;
 
         try {
-            connection = DriverManager.getConnection(url, user,password);
+            connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection Open");
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e);
         }
 
-        try {
-            statement = connection.createStatement();
-            PreparedStatement requete = connection.prepareStatement("SELECT * FROM  login WHERE user = ? and password = ?");
-            requete.setString(1,nom);
-            requete.setString(2,pass);
-            resultat = requete.executeQuery();
-            if (resultat.next()){
-                action=true;
-            }
-        }
-        catch (SQLException e) {
-            System.out.println("Request Error");
-        }
+//        try {
+//            statement = connection.createStatement();
+//            PreparedStatement requete = connection.prepareStatement("SELECT * FROM  login WHERE user = ? and password = ?");
+//            requete.setString(1,nom);
+//            requete.setString(2,pass);
+//            resultat = requete.executeQuery();
+//            if (resultat.next()){
+//                action=true;
+//            }
+//        }
+//        catch (SQLException e) {
+//            System.out.println("Request Error");
+//        }
 
         finally {
-            if ( resultat != null) {
-                resultat.close();
-            }
-            if ( statement != null){
-                resultat.close();
-            }
-            if ( connection != null) {
+//            if ( resultat != null) {
+//                resultat.close();
+//            }
+//            if ( statement != null){
+//                resultat.close();
+//            }
+            if (connection != null) {
                 connection.close();
             }
         }
-        return action;
     }
 }
 
