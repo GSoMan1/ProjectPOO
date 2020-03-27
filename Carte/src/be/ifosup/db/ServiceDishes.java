55060382a3dd4d36b442a60989abfd153cde3ff7
Kinoxbe <56653418+Kinoxbe@users.noreply.github.com>
2020-03-27 15:12:33 +0100

    Test sql request to arraylist

--- Carte/src/be/ifosup/db/ServiceDishes.java
+++ Carte/src/be/ifosup/db/ServiceDishes.java
@@ -1,5 +1,48 @@
 package be.ifosup.db;
 
-public class ServiceDishes {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.ConsoleHandler;
+
+public class ServiceDishes  {
+    private static ConsoleHandler resultat;
     //todo
+
+    public List<List<String>> GetDishes() throws SQLException {
+
+        List<List<String>> dishes = new ArrayList<>();
+//    private Statement stmt;
+        Connection connection = DbDAO.initializeDatabase();
+
+        try {
+//          Statement statement = connection.createStatement();
+            PreparedStatement requete = connection.prepareStatement("SELECT * FROM dishes");
+            ResultSet resultat = requete.executeQuery();
+            ResultSetMetaData rsmd = resultat.getMetaData();
+            while (resultat.next()) {
+                List<String> Dish = new ArrayList<>();
+                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                    Dish.add(resultat.getString(i));
+                }
+                dishes.add(Dish);
+            }
+        } catch (SQLException e) {
+            System.out.println("Request Error");
+        }
+
+        finally {
+            if (resultat != null) {
+                resultat.close();
+            }
+//            if (statement != null) {
+//                resultat.close();
+//            }
+            if (connection != null) {
+                connection.close();
+            }
+
+        }
+        return dishes;
+    }
 }
