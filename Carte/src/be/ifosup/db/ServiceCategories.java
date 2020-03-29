8c045cc66fb8ff76eaac3d3301be9d4cac74aa2f
MaxLevis <58523941+MaxLevis@users.noreply.github.com>
2020-03-29 16:11:36 +0200

    Creation ServiceCategory
    2 fonctions pour liste complète et catégorie par id.

--- Carte/src/be/ifosup/db/ServiceCategories.java
+++ Carte/src/be/ifosup/db/ServiceCategories.java
@@ -1,5 +1,78 @@
 package be.ifosup.db;
 
+import be.ifosup.entities.Category;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.ConsoleHandler;
+
 public class ServiceCategories {
-    //todo
+    private static ConsoleHandler resultat;
+
+    public static List<Category> GetCategory() throws SQLException {
+
+        List<Category> categories = new ArrayList<Category>();
+        Connection connection = DbDAO.initializeDatabase();
+
+        try {
+
+            PreparedStatement requete = connection.prepareStatement("SELECT * FROM category");
+            ResultSet resultat = requete.executeQuery();
+
+            while(resultat.next()){
+                String name = resultat.getString("name");
+                int id = resultat.getInt("id");
+                Category category = new Category(id, name);
+                categories.add(category);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Request Error");
+        }
+
+        finally {
+            if (resultat != null) {
+                resultat.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+
+        }
+        return categories;
+    }
+
+    public static Category GetCategoryByID(int catid) throws SQLException {
+
+        Connection connection = DbDAO.initializeDatabase();
+
+        Category category = null;
+        try {
+            PreparedStatement requete = connection.prepareStatement("SELECT * FROM category WHERE id="+catid);
+            ResultSet resultat = requete.executeQuery();
+
+
+            if (resultat.next()) {
+                String name = resultat.getString("name");
+                int id = resultat.getInt("id");
+                category = new Category(id, name);
+            }else{
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Request Error");
+        } finally {
+            if (resultat != null) {
+                resultat.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+
+        }
+        return category;
+    }
 }
