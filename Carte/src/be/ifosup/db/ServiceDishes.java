a4dc4d30f6d6b18e4f3994583e5df1a59b0c7520
MaxLevis <58523941+MaxLevis@users.noreply.github.com>
2020-03-29 16:14:47 +0200

    Update ServiceDishes
    Ajout du choix par catégorie

--- Carte/src/be/ifosup/db/ServiceDishes.java
+++ Carte/src/be/ifosup/db/ServiceDishes.java
@@ -6,24 +6,31 @@ import java.util.List;
 import java.util.logging.ConsoleHandler;
 import be.ifosup.entities.Dish;
 
+
 public class ServiceDishes  {
     private static ConsoleHandler resultat;
     //todo
 
-    public static List<Dish> GetDishes() throws SQLException {
+    public static List<Dish> GetDishes(int catid) throws SQLException {
 
         List<Dish> dishes = new ArrayList<Dish>();
         Connection connection = DbDAO.initializeDatabase();
 
         try {
-            PreparedStatement requete = connection.prepareStatement("SELECT * FROM dishes");
+            String statement ="";
+            if (catid == 0) {
+                statement = "SELECT * FROM dishes";
+            }else{
+                statement = "SELECT * FROM dishes WHERE catid ="+catid;
+            }
+            PreparedStatement requete = connection.prepareStatement(statement);
             ResultSet resultat = requete.executeQuery();
-            ResultSetMetaData rsmd = resultat.getMetaData();
+
 
             while(resultat.next()){
                 String title = resultat.getString("title");
                 String description = resultat.getString("description");
-                float price = resultat.getFloat("price");
+                Float price = resultat.getFloat("price");
                 int id = resultat.getInt("id");
                 Dish dish = new Dish(title,description,price,id);
                 dishes.add(dish);
@@ -37,12 +44,11 @@ public class ServiceDishes  {
             if (resultat != null) {
                 resultat.close();
             }
+
             if (connection != null) {
                 connection.close();
             }
-
         }
         return dishes;
     }
 }
-
